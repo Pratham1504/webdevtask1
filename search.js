2ea756128c9e61f4d3dfaf05a2f6a3552ba78731
Pratham1504 <prathamgoyal1504@gmail.com>
2022-06-02 16:43:52 +0530

    Nothing

---
+++ search.js
@@ -0,0 +1,43 @@
+const charactersList = document.getElementById('charactersList');
+const searchBar = document.getElementById('searchBar');
+let hpCharacters = [];
+
+searchBar.addEventListener('keyup', (e) => {
+    const searchString = e.target.value.toLowerCase();
+
+    const filteredCharacters = hpCharacters.filter((character) => {
+        return (
+            character.title.toLowerCase().includes(searchString) ||
+            character.category.toLowerCase().includes(searchString)
+        );
+    });
+    displayCharacters(filteredCharacters);
+});
+
+const loadCharacters = async () => {
+    try {
+        const res = await fetch('products.txt');
+        hpCharacters = await res.json();
+        displayCharacters(hpCharacters);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+const displayCharacters = (characters) => {
+    const htmlString = characters
+        .map((character) => {
+            return `
+            <li class="character">
+                <h2>${character.title}</h2>
+                <h3>Price:$ ${character.price}-${character.discountPercentage}%</h3>
+                <p>${character.description}</p>
+                <img src="${character.thumbnail}"></img>
+            </li>
+        `;
+        })
+        .join('');
+    charactersList.innerHTML = htmlString;
+};
+
+loadCharacters();
